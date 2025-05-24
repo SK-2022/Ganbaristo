3109fbed896e19718b00ded2c723e6e7c9c636b5
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-05-24 19:52:58 +0900

    Test the project class functionality

--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@
 import "/src/styles/main.css";
 
 import ToDo  from "./modules/todo.js";
+import Project from "./modules/project.js";
 
 const importantTask = new ToDo(
     "Incredibly Important task",
-    "This task must be done ASAP",
+    "Work on the Javascript Section on The Odin Project",
     "3rd of May",
     "High priority",
     ["Bonus task 1", "Bonus task 2"],
     false,
 )
 
-//Testing the functionality. All seems good
+//Testing the functionality of the ToDo Class. Passed!
 importantTask.toggleCompletion()
-importantTask.addChecklistItem("Dance with Rina")
+importantTask.addChecklistItem("Talk to Mr Maeda about your projects especially Javascript")
 importantTask.priority = "Low priority"
 console.log(importantTask)
+
+//Testing the functionality of the Project Class. Passed!
+const theOdinProject = new Project("JavaScript Section", [])
+
+theOdinProject.addToDoListItem(importantTask)
+
+console.log(theOdinProject)
