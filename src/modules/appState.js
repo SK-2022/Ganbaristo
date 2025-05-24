61539c9d6b9ae54585a812aae905fff734bd86b0
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-05-24 21:24:11 +0900

    Add methods for saving and retrieving data to and from localStorage

--- src/modules/appState.js
+++ src/modules/appState.js
@@ -1,16 +1,30 @@
 //This is a central place to store all projects and handle app-wide actions like adding/deleting a project or saving to localStorage.
-export default class AppStateManager { 
-    constructor(projectsArray) {
-      this.projectsArray = projectsArray
-    }
 
-    //Add a project to the AppStateManager
-    addToProjectsArray(project) {
-      this.projectsArray.push(project)
-    }
+export default class AppStateManager {
+  constructor(projectsArray) {
+    this.projectsArray = projectsArray;
+  }
 
-    //Remove a project from the AppStateManager
-    removeFromProjectsArray(index) {
-      this.projectsArray.splice(index, 1)
-    }
+  //Add a project to the AppStateManager
+  addToProjectsArray(project) {
+    this.projectsArray.push(project);
+    this.saveToLocalStorage();
+  }
+
+  //Remove a project from the AppStateManager
+  removeFromProjectsArray(index) {
+    this.projectsArray.splice(index, 1);
+    this.saveToLocalStorage();
+  }
+
+  //Save the projects array (and todos) to localStorage
+  saveToLocalStorage() {
+    localStorage.setItem("projects", JSON.stringify(this.projectsArray));
+  }
+
+  //Retrieve the projects array from localStorage, always return an array
+  getProjectsArray() {
+    const loadedProjects = JSON.parse(localStorage.getItem("projects"));
+    return Array.isArray(loadedProjects) ? loadedProjects : [];
+  }
 }
