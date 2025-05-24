2451b798e1134b8c277505ff599ed9e4a218f0d1
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-05-24 20:34:39 +0900

    Add reason for app creation as well as AppStateManager methods for removing and adding projects

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Ganbaristo is a to do list. Ganbaru(頑張る) and risuto(リスト) equals Ganbaristo
+
+I use a to do list everyday and its about time I make one for myself.
--- src/modules/appState.js
+++ src/modules/appState.js
@@ -1,1 +1,16 @@
 //This is a central place to store all projects and handle app-wide actions like adding/deleting a project or saving to localStorage.
+export default class AppStateManager { 
+    constructor(projectsArray) {
+      this.projectsArray = projectsArray
+    }
+
+    //Add a project to the AppStateManager
+    addToProjectsArray(project) {
+      this.projectsArray.push(project)
+    }
+
+    //Remove a project from the AppStateManager
+    removeFromProjectsArray(index) {
+      this.projectsArray.splice(index, 1)
+    }
+}
