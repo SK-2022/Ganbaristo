5c6fe831c573d17b5fd8565bf80a8e192c0f35db
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-05-27 08:30:25 +0900

    Implement the renderTodosList function

--- src/dom/render.js
+++ src/dom/render.js
@@ -1,10 +1,8 @@
-import AppStateManager from "../modules/appState";
-import ToDo from "../modules/todo";
-import Project from "../modules/project";
-import ChecklistItem from "../modules/checklist";
-
 const addTaskButton = document.querySelector(".add-task-button");
 const projectList = document.querySelector(".project-list");
+const todosListParentContainer = document.querySelector(
+  ".to-do-list-parent-container"
+);
 
 //Go through the projects array and display them in the .project-list element.
 
@@ -12,8 +10,8 @@ export function renderProjectList(projectsArray) {
   //Clear the current list to avoid duplicates
   projectList.innerHTML = "";
 
-  //Loop through each projecet and create a list item
-  projectsArray.forEach(project => {
+  //Loop through each project and create a list item
+  projectsArray.forEach((project) => {
     const li = document.createElement("li");
     li.classList.add("project-list-item");
 
@@ -22,12 +20,35 @@ export function renderProjectList(projectsArray) {
     anchorTag.href = "#";
     anchorTag.textContent = project.name;
 
-    //Append the anchor to the list itme, and the list item to the project list.
+    //Append the anchor to the list item, and the list item to the project list.
     li.appendChild(anchorTag);
     projectList.appendChild(li);
   });
 }
 
-//How to test?
-//Create a project array and STORE IT somewhere in a variable(Appstatemanager?)
-//Access the project array through my renderProjectList function. 
+// Go through todos array and display them in the
+export function renderTodosList(todosArray) {
+  //Clear the current list to avoid duplicates
+  todosList.innerHTML = "";
+
+  //Loop through each to do and create a div for it
+  todosArray.forEach((todo) => {
+    //Create the todo item container
+    const todoListContainer = document.createElement("div");
+    todoListContainer.classList.add("to-do-list-container");
+
+    //Create the checklist button for each to do item
+    const checklistButton = document.createElement("button");
+    checklistButton.add.classList("checklist-button");
+
+    //Create the todo title div for the title.
+    const todoTitleDiv = document.createElement("div");
+    todoTitleDiv.classList.add("to-do-title");
+    todoTitleDiv.textContent = todo.title;
+
+    // Add the checklist button and title to the todo container, then append it to the parent container
+    todoListContainer.appendChild(checklistButton);
+    todoListContainer.appendChild(todoTitleDiv);
+    todosListParentContainer.appendChild(todoListContainer);
+  });
+}
--- src/modules/checklist.js
+++ src/modules/checklist.js
@@ -14,3 +14,5 @@ export default class ChecklistItem {
         return this.done
     }
 }
+
+//HAVE A COUNTER FOR YOUR CHECKLIST ITEMS AS SUBTASKS
