48e41ea371dcc3ac9e5f383265ae074aa77f9f41
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-05-27 07:40:56 +0900

    Implement the renderProjectList function to display project lists on the DOM sidebar

--- src/dom/render.js
+++ src/dom/render.js
@@ -3,11 +3,27 @@ import ToDo from "../modules/todo";
 import Project from "../modules/project";
 import ChecklistItem from "../modules/checklist";
 
-const addTaskButton = document.querySelector('.add-task-button')
-const projectList = document.querySelector('.project-list')
+const addTaskButton = document.querySelector(".add-task-button");
+const projectList = document.querySelector(".project-list");
 
-function renderProjectList(projectsArray){
-    projectsArray.forEach(project => {
-        
-    });
+//Go through the projects array and display them in the .project-list element.
+
+function renderProjectList(projectsArray) {
+  //Clear the current list to avoid duplicates
+  projectList.innerHTML = "";
+
+  //Loop through each projecet and create a list item
+  projectsArray.forEach((project) => {
+    const li = document.createElement("li");
+    li.classList.add("project-list-item");
+
+    //Create an anchor tag for the project name
+    const anchorTag = document.createElement("a");
+    anchorTag.href = "#";
+    anchorTag.textContent = project.name;
+
+    //Append the anchor to the list itme, and the list item to the project list.
+    li.appendChild(a);
+    projectList.appendChild(li);
+  });
 }
