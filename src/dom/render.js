598ca7b293dfa81b74e959489d00d4d55b7673a9
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-05-27 07:57:27 +0900

    Export renderProjectList function and integrate it into the app entry point

--- src/dom/render.js
+++ src/dom/render.js
@@ -8,12 +8,12 @@ const projectList = document.querySelector(".project-list");
 
 //Go through the projects array and display them in the .project-list element.
 
-function renderProjectList(projectsArray) {
+export function renderProjectList(projectsArray) {
   //Clear the current list to avoid duplicates
   projectList.innerHTML = "";
 
   //Loop through each projecet and create a list item
-  projectsArray.forEach((project) => {
+  projectsArray.forEach(project => {
     const li = document.createElement("li");
     li.classList.add("project-list-item");
 
@@ -23,7 +23,11 @@ function renderProjectList(projectsArray) {
     anchorTag.textContent = project.name;
 
     //Append the anchor to the list itme, and the list item to the project list.
-    li.appendChild(a);
+    li.appendChild(anchorTag);
     projectList.appendChild(li);
   });
 }
+
+//How to test?
+//Create a project array and STORE IT somewhere in a variable(Appstatemanager?)
+//Access the project array through my renderProjectList function. 
