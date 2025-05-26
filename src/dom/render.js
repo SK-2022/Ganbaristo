017b015c167c6a1db97c23a53453151b4b7b4249
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-05-26 22:18:40 +0900

    Implement project list rendering and update HTML structure for project list

---
+++ src/dom/render.js
@@ -0,0 +1,13 @@
+import AppStateManager from "../modules/appState";
+import ToDo from "../modules/todo";
+import Project from "../modules/project";
+import ChecklistItem from "../modules/checklist";
+
+const addTaskButton = document.querySelector('.add-task-button')
+const projectList = document.querySelector('.project-list')
+
+function renderProjectList(projectsArray){
+    projectsArray.forEach(project => {
+        
+    });
+}
