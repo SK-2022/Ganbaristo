3a155aa1285b800833ce6ac8d582fbc9fddeb6be
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-05-27 09:30:20 +0900

    Enhance renderTodosList function to include todo description, due date, and priority; remove hardcoded checklist items from template

--- src/dom/render.js
+++ src/dom/render.js
@@ -30,7 +30,7 @@ export function renderProjectList(projectsArray) {
 export function renderTodosList(todosArray) {
   //Clear the current list to avoid duplicates
   todosListParentContainer.innerHTML = "";
-  
+
   //Loop through each to do and create a div for it
   todosArray.forEach((todo) => {
     //Create the todo item container
@@ -38,22 +38,45 @@ export function renderTodosList(todosArray) {
     todoListContainer.classList.add("to-do-list-container");
 
     //Create the checklist button for each to do item
+    //COME BACK HERE LATER FOR CHECKLIST FUNCTIONALITY
     const checklistButton = document.createElement("button");
     checklistButton.classList.add("checklist-button");
-    checklistButton.textContent = "done"
+    checklistButton.textContent = "done";
 
-    //Create the todo title div for the title.
+    //Create the todo title container for the title.
     const todoTitleDiv = document.createElement("div");
     todoTitleDiv.classList.add("to-do-title");
     todoTitleDiv.textContent = todo.title;
 
+    //Create the todo description container
+    const todoDescription = document.createElement("div");
+    todoDescription.classList.add("to-do-description");
+    todoDescription.textContent = todo.description;
+
+    //Create the todo date and priority parent container
+    const dateAndPriorityContainer = document.createElement("div");
+    dateAndPriorityContainer.classList.add("date-and-priority-container");
+
+    //Create the todo date container
+    const todoDate = document.createElement("div");
+    todoDate.classList.add("to-do-date");
+    todoDate.textContent = todo.dueDate;
+
+    //Create the todo priority container
+    const todoPriority = document.createElement("div");
+    todoPriority.classList.add("to-do-priority");
+    todoPriority.textContent = todo.priority;
+
     // Add the checklist button and title to the todo container, then append it to the parent container
     todoListContainer.appendChild(checklistButton);
     todoListContainer.appendChild(todoTitleDiv);
+    todoListContainer.appendChild(todoDescription);
+
+    todoListContainer.appendChild(dateAndPriorityContainer);
+    dateAndPriorityContainer.appendChild(todoDate);
+    dateAndPriorityContainer.appendChild(todoPriority);
     todosListParentContainer.appendChild(todoListContainer);
   });
 }
 
-export function renderChecklistItems(checklist) {
-
-}
+export function renderChecklistItems(checklist) {}
