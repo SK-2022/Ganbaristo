ee74fa9e26b090284bde108cac5e270e3becf4c5
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-10-23 10:59:23 +0900

    refactor: remove unused ToDo item creation and related imports to streamline project initialization

--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
 // App entry point
 import './styles/main.css';
-import ToDo, { prioritySelectionsArray } from './modules/todo.js';
 import Project from './modules/project.js';
 import appStateManager from './modules/appState.js';
 import { renderProjectList, renderTodosList } from './dom/render.js';
 import initEventListeners from './dom/events.js';
 // import { renderChecklistItems } from './dom/render.js';
 
-// Create initial todo list test item
-const importantTask = new ToDo(
-  'Incredibly Important task',
-  'Work on the Javascript Section on The Odin Project',
-  new Date(),
-  prioritySelectionsArray[2],
-  false,
-);
-
 const theOdinProject = new Project('The Odin Project', []);
 const theFitnessProject = new Project('The Fitness Project', []);
-theOdinProject.addToDoListItem(importantTask);
 
 appStateManager.addToProjectsArray(theOdinProject);
 appStateManager.addToProjectsArray(theFitnessProject);
 // Load from localStorage when the app starts. Console.log() first. Add DOM elements later
 console.log(appStateManager.getProjectsArray());
 
-console.log(importantTask);
 console.log(theOdinProject);
 console.log(appStateManager.getProjectsArray());
 
